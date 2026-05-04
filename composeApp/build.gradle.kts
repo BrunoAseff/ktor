e7852e7b825b01b02dbc20943cd64f0178698f0e
Bruno de Almeida Aseff <7762062@univali.br>
2026-05-04 20:13:41 -0300

    fix: adjust build

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,8 +43,8 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtimeCompose)
             
             implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.content-negotiation)
-            implementation(libs.ktor.serialization-kotlinx-json)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
         }
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
